59e304d6b0a286387f091aaf3b604e303d7d7e8d
Farchi <farcas683@gmail.com>
2023-03-08 10:37:35 +0200

    start about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,41 @@
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from 'react-intersection-observer'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../variants'
 
-const About = () =>{
-    return <div className='section' id='about'>About</div>
+const About = () => {
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+    });
+    return (
+        <section className='section' id='about' ref={ref}>
+            <div className="container mx-auto">
+
+                <div className="flex-1 bg-about bg-contain bg-no-repeat h-[650px] mix-lend-lighten bg-top"></div>
+
+                <div>
+                    <h2>About me.</h2>
+                    <h3>
+                        I'm a Front-End Developer on the begginning of the road.
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aperiam explicabo aliquid vero earum cumque nam ex, alias hic suscipit quisquam? 
+                    </p>
+                    <div>
+                        <div>
+                            <CountUp start={0} end={13} duration={3}/>
+                        </div>
+                        <div className="font-primary text-sm tracking-[2px]">
+                            Years of <br/>
+                            Experience
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -22,7 +22,7 @@ const Banner = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
                     <div className="flex-1 text-center font-secondary lg:text-left">
-                        <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="text-[55px] font-bold leading-[0.8] lg:text-[70px]">ALEX <span>FARCAS</span>
+                        <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="text-[65px] font-bold leading-[0.8] lg:text-[70px]">ALEX <span>FARCAS</span>
                         </motion.h1>
                         <motion.div variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
                             <span className="mr-4 text-white">I am a</span>
